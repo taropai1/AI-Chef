7588d9582ddade9854354234e222779eadc34431
taropai <ls@tarop.top>
2026-05-02 14:14:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1047,6 +1047,8 @@ function switchMode(mode) {
             desc.classList.remove('left-indent');
         }
     }
+    const sendBtn = document.getElementById('btnGenerate');
+if (sendBtn) sendBtn.innerHTML = `<svg viewBox="0 0 24 24"><line x1="22" y1="2" x2="11" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>`;
 }
 
 function toggleDropdown(type) {
