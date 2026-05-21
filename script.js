ded6d5fff3ea4e7011fc01dc4dafbc8397d590fa
taropai <ls@tarop.top>
2026-05-22 07:10:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1261,11 +1261,6 @@ function initAiPage() {
 if (videoBtn) {
     videoBtn.textContent = t('videoShort') || 'Videos';
     videoBtn.onclick = function() {
-        if (!userData) {
-            alert(t('pleaseLogin'));
-            showPage('page-login-register');
-            return;
-        }
         showPage('page-video');
     };
 }
